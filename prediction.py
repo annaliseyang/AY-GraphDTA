696726a35a4087ed8f39b3a57425bca1e1d2684c
Anna Yang <anna_y@mit.edu>
2024-06-07 03:39:22 -0400

    refined predictions

--- prediction.py
+++ prediction.py
@@ -7,30 +7,9 @@ import os
 from create_data import *
 from utils import *
 from models.ginconv import GINConvNet
+import requests
+import pubchempy as pcp
 
-# Load the trained model
-with open('model_GINConvNet_davis.model', 'rb') as file:
-    model = torch.load(file)
-
-# Prepare the input data
-# write a csv file with the data
-dataset = pd.DataFrame()
-features = ['compound_iso_smiles', 'target_sequence', 'affinity']
-
-# dictionaries mapping SMILES to PubChem ID and drug name
-drugs = {
-    'CN(C)C1=CC2=C(C=C1)N=C3C=CC(=[N+](C)C)C=C3S2.[Cl-]': ('methylene blue', '6099'),
-    'COC1=C(C=CC(=C1)C=CC(=O)CC(=O)C=CC2=CC(=C(C=C2)O)OC)O': ('Curcumin', '969516'),
-    'CN1CCC23C=CC(CC2OC4=C(C=CC(=C34)C1)OC)O.Br': ('Galantamine Hydrobromide', '121587'),
-    'CN(C)CC1=NC2=C(C=C1)C(=CC(=C2O)Cl)Cl': ('5,7-dichloro-2-[(dimethylamino)methyl]quinolin-8-ol', '10016012'),
-    'CCCN(CCN1CCN(CC1)C2=CC=C(C=C2)C3=CC(=C(C=C3)O)O)C4CCC5=C(C4)SC(=N5)N': ('Tau-aggregation-IN-1', '163408861'),
-    'C1CN(CCN1C2=CC(=C(C=C2)[N+](=O)[O-])NC3=CC=CC=C3)C(=O)C4=CC=CC=C4': ('Abeta/tau aggregation-IN-3', '44814403'),
-}
-
-drugs2 = {
-    'CC(=O)OC1=CC=CC=C1C(=O)O': ('Aspirin', '2244'),
-    'C(C1C(C(C(C(O1)O)O)O)O)O': ('D-Glucose', '5793'),
-}
 
 def load_fasta(file):
     try:
@@ -40,13 +19,11 @@ def load_fasta(file):
     except Exception as e:
         print(f"Error loading fasta file: {e}")
 
-target = '5O3L' # tau protein paired helical filament in Alzheimer's disease brain
-target_sequence = load_fasta(f'data/prediction_data/rcsb_pdb_{target}.fasta') # load the fasta file
-target_sequence = Bio.SeqIO.read(f'data/prediction_data/rcsb_pdb_{target}.fasta', 'fasta').seq
-print(f"Target sequence: {str(target_sequence)}")
 
 def create_data(drugs, target_sequence, dataset='input'):
-    # Create dataframe with input data
+    """
+    Create dataframe with input data
+    """
     filename = f'data/{dataset}_data.csv'
     df = pd.DataFrame(columns=['compound_iso_smiles', 'target_sequence'])
 
@@ -55,12 +32,72 @@ def create_data(drugs, target_sequence, dataset='input'):
         df = df._append({
             'compound_iso_smiles': smiles,
             'target_sequence': target_sequence,
-            # 'affinity': 0.0  # Placeholder for affinity
         }, ignore_index=True)
 
     # save data to csv
     df.to_csv(filename, index=False)
     return df
+
+
+def pubchem_id_lookup(pubchem_id):
+    """
+    Get the name and SMILES of a molecule given its PubChem ID
+    """
+    try:
+        compound = pcp.Compound.from_cid(pubchem_id)
+        try: # try to get the first synonym
+            return compound.synonyms[0]
+        except IndexError: # if no synonyms found, return the IUPAC name
+            return compound.iupac_name
+    except Exception as e:
+        print(f"Error looking up PubChem ID {pubchem_id}: {e}")
+        return None
+
+
+def get_name_from_chembl_id(chembl_id):
+    url = f"https://www.ebi.ac.uk/chembl/api/data/molecule/{chembl_id}.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        try: # check for preferred name
+            return data['pref_name']
+        except KeyError:
+            try: # check for synonyms
+                return data['molecule_synonyms'][0]['molecule_synonym']
+            except KeyError: # return CHEMBL id if no preferred name or synonyms found
+                return chembl_id
+    else:
+        return None
+
+
+def get_ligands_from(reference_dataset='davis'):
+    """
+    Returns a dictionary mapping drug SMILES to PubChem ID and drug name from the given dataset
+    Saves the data to a csv file
+    """
+    if os.path.exists(f'data/{reference_dataset}/ligands_info.csv'):
+        df = pd.read_csv(f'data/{reference_dataset}/ligands_info.csv')
+        out = {}
+        for i, row in df.iterrows():
+            out[row['Canonical SMILES']] = row['Name'], row['PubChem ID']
+        return out
+
+    ligands = json.load(open(f"data/{reference_dataset}/ligands_can.txt"), object_pairs_hook=OrderedDict)
+
+    out = {}
+    df = pd.DataFrame(columns=['PubChem ID', 'Name', 'Canonical SMILES'])
+    for id, ligand in ligands.items():
+        name = pubchem_id_lookup(id) if reference_dataset == 'davis' else id
+        out[ligand] = name, id
+        df = df._append({
+            'PubChem ID': id,
+            'Name': name,
+            'Canonical SMILES': ligand
+        }, ignore_index=True)
+
+    df.to_csv(f'data/{reference_dataset}/ligands_info.csv', index=False)
+
+    return out
 
 
 def load_csv(filename):
@@ -72,7 +109,7 @@ def load_csv(filename):
         print(f"Error loading data: {e}")
 
 
-def preprocess_data(df=None, dataset='input', multi_target=False):
+def preprocess_data(df=None, dataset='input', multi_target=True):
     """
     Preprocess the input data
     Input data is a csv file with columns: 'compound_iso_smiles', 'target_sequence'
@@ -94,6 +131,9 @@ def preprocess_data(df=None, dataset='input', multi_target=False):
 
 
 class EvaluationDataset(TestbedDataset):
+    """
+    Class for the evaluation dataset, inheriting from the TestbedDataset class
+    """
     def __init__(self, root='/tmp', dataset='input', drugs=None, prots=None,
                  xd=None, xt=None, y=None, transform=None,
                  pre_transform=None,smile_graph=None):
@@ -130,6 +170,7 @@ class EvaluationDataset(TestbedDataset):
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
 
+
 def make_prediction(model, device, loader):
     model.eval()
     total_preds = torch.Tensor()
@@ -142,7 +183,7 @@ def make_prediction(model, device, loader):
             total_preds = torch.cat((total_preds, output.cpu()), 0)
     return total_labels, total_preds.numpy().flatten()
 
-# make a dataframe with the predictions
+
 def save_as_csv(df, predictions, data: EvaluationDataset):
     exp, preds = predictions
     assert len(df) == len(preds), f"The number of predictions do not match the number of samples: {len(preds)} predictions vs. {len(df)} samples"
@@ -155,13 +196,16 @@ def save_as_csv(df, predictions, data: EvaluationDataset):
         new_df['expected_affinity'] = exp
 
     new_df['predicted_affinity'] = preds
-    print(f"\nPredictions for dataset '{data.dataset}':")
-    print(new_df.head(10))
+
+    num_best_results = 10
+    print(f"\nTop {min(len(new_df), num_best_results)} out of {len(new_df)} predictions for dataset '{data.dataset}' sorted by affinity:")
+    print(new_df.nlargest(num_best_results, 'predicted_affinity'))
 
     best_result = new_df.loc[new_df['predicted_affinity'].idxmax()]
     name, id = data.drugs.get(best_result['compound_iso_smiles'])
-    print(f"\nMaximum predicted affinity found for '{name}' (PubChem id {id}): {np.max(preds)}")
-    print(f"Average predicted affinity: {np.mean(preds)}")
+    print(f"\nBest binding result:\tligand '{name}' ({'PubChem' if data.dataset == 'davis' else 'CHEMBL'} id: {id}) and target {target}")
+    print(f"Maximum predicted affinity = {np.max(preds)}")
+    print(f"Average predicted affinity = {np.mean(preds)}")
 
     filename = f'predictions/{data.dataset}_prediction.csv'
     print(f"\nPredictions saved to file: {filename}")
@@ -172,12 +216,38 @@ def save_as_csv(df, predictions, data: EvaluationDataset):
 
 
 if __name__ == '__main__':
-    # Create the input data
-    datasets = {
-        'input': drugs,
-        'input2': drugs2,
+    # Create the input data (ligands and target sequence)
+    # dictionaries mapping SMILES to PubChem ID and drug name
+    drugs = {
+        'CN(C)C1=CC2=C(C=C1)N=C3C=CC(=[N+](C)C)C=C3S2.[Cl-]': ('methylene blue', '6099'),
+        'COC1=C(C=CC(=C1)C=CC(=O)CC(=O)C=CC2=CC(=C(C=C2)O)OC)O': ('Curcumin', '969516'),
+        'CN1CCC23C=CC(CC2OC4=C(C=CC(=C34)C1)OC)O.Br': ('Galantamine Hydrobromide', '121587'),
+        'CN(C)CC1=NC2=C(C=C1)C(=CC(=C2O)Cl)Cl': ('5,7-dichloro-2-[(dimethylamino)methyl]quinolin-8-ol', '10016012'),
+        'CCCN(CCN1CCN(CC1)C2=CC=C(C=C2)C3=CC(=C(C=C3)O)O)C4CCC5=C(C4)SC(=N5)N': ('Tau-aggregation-IN-1', '163408861'),
+        'C1CN(CCN1C2=CC(=C(C=C2)[N+](=O)[O-])NC3=CC=CC=C3)C(=O)C4=CC=CC=C4': ('Abeta/tau aggregation-IN-3', '44814403'),
     }
 
+    control = {
+        'CC(=O)OC1=CC=CC=C1C(=O)O': ('Aspirin', '2244'),
+        'C(C1C(C(C(C(O1)O)O)O)O)O': ('D-Glucose', '5793'),
+    }
+
+    all_davis = get_ligands_from('davis')
+    all_kiba = get_ligands_from('kiba')
+
+    datasets = {
+        'input': drugs,
+        'control': control,
+        'davis': all_davis,
+        'kiba': all_kiba,
+    }
+
+    # Load target sequence
+    target = '5O3L' # tau protein paired helical filament in Alzheimer's disease brain
+    target_sequence = Bio.SeqIO.read(f'data/test_targets/rcsb_pdb_{target}.fasta', 'fasta').seq
+    print(f"\n{target} sequence: {target_sequence}")
+
+    # Process and save each dataset, then predict binding affinities for each drug-target pair
     for dataset, drugs in datasets.items():
         print(f"------------------------------------------------------------")
         df = create_data(drugs, target_sequence, dataset)
@@ -188,13 +258,13 @@ if __name__ == '__main__':
             g = smile_to_graph(smile)
             smile_graph[smile] = g
 
-        drugs, prots = preprocess_data(df)
-        # print("Preprocessed data:", drugs, prots)
+        # Preprocess the data
+        drugs, prots = preprocess_data(df, dataset, multi_target=False)
         processed_data = EvaluationDataset(root='data', dataset=dataset, drugs=datasets[dataset], prots=prots, xd=drugs, xt=prots, smile_graph=smile_graph)
 
         # Prepare model
         model_state_dict = torch.load('model_GINConvNet_davis.model', map_location='cpu')
-        model = GINConvNet()  # Replace YourModelClass with the class of your model
+        model = GINConvNet() # Create a new model instance
         model.load_state_dict(model_state_dict)
 
         # Make predictions
